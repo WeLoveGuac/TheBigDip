6c48cd353f42b9d2768f8df73ab5d77e5a0d4503
smart1206 <proharvester@outlook.com>
2022-08-16 06:57:09 -0500

    dip chip modals

--- components/Dip.js
+++ components/Dip.js
@@ -1,9 +1,20 @@
-import { makeStyles } from "@material-ui/core/styles";
-import { Container, Typography, Box, Grid, Button } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { Container, Typography, Box, Grid, Button, Backdrop, Modal, Fade, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from "@material-ui/core";
 import { useSelector } from 'react-redux';
 import clsx from "clsx";
 import { useState } from "react";
 import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const CustomRadio = withStyles({
+  root: {
+    color: "#c5cab8",
+    '&$checked': {
+      color: "#fff9ad",
+    },
+  },
+  checked: {},
+})((props) => <Radio color="default" {...props} />);
 
 const useStyles = makeStyles((theme) => ({
   dip: {
@@ -26,13 +37,13 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: '0px',
     },
     cursor: 'pointer',
-    "&:hover img:first-child": {
-      zIndex: 30,
+    "&:hover img:nth-child(1)": {
+      zIndex: 2,
       WebkitTransform: 'scale(1.5)',
       transform: 'scale(1.5)',
     },
-    "&:hover img:last-child": {
-      zIndex: 20,
+    "&:hover img:nth-child(2)": {
+      zIndex: 1,
       WebkitTransform: 'scale(2)',
       transform: 'scale(2)',
     },
@@ -45,6 +56,42 @@ const useStyles = makeStyles((theme) => ({
     color: '#fff9ad',
     marginTop: '100px',
     textAlign: 'center'
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    width: "450px",
+    minHeight: '350px',
+    border: '5px solid #000f47',
+    boxShadow: theme.shadows[5],
+    padding: "30px",
+    background: "#4e6dd1",
+    borderRadius: "20px",
+    outline: 0,
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'column'
+  },
+  selectModalTitle: {
+    color: '#fff9ad',
+    fontSize: '20px',
+    textAlign: 'center',
+    marginBottom: '20px'
+  },
+  customLabel: {
+    fontWeight: "bold",
+    fontSize: "16px",
+    lineHeight: "1.5",
+    color: "#001446",
+  },
+  chipTitle: {
+    fontWeight: "bold",
+    fontSize: "25px",
+    textTransform: 'uppercase',
+    lineHeight: "1.5"
   }
 }));
 
@@ -55,32 +102,118 @@ const Dip = () => {
     balance
   } = useSelector(state => state.ConnectWallet);
 
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const imgArr = [
     {
+      id: 'queso',
       url1: 'dipchip1.png',
-      url2: 'quesobowl.png'
+      url2: 'quesobowl.png',
+      color: '#d29f4d'
     },
     {
+      id: 'guac',
       url1: 'dipchip2.png',
-      url2: 'guacbowl.png'
+      url2: 'guacbowl.png',
+      color: '#71cf2d'
     },
     {
+      id: 'salsa',
       url1: 'dipchip3.png',
-      url2: 'salsabowl.png'
+      url2: 'salsabowl.png',
+      color: '#500912'
     }
   ]
+
+  const [value, setValue] = useState(null); // choosen chip number
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const [dipped, setDipped] = useState(false)
+
+  const dipChip = () => {
+    value && setDipped(true);
+    !value && toast.error(`Choose one Dip!`);
+  }
+
+  const [selectedDip, setSelectedDip] = useState();
+
+  const chooseDip = (event) => {
+    event.preventDefault();
+    setSelectedDip(event.currentTarget.id)
+    setOpen(true)
+    setValue(null);
+    setDipped(false);
+  }
 
   return (
     <Container>
       <Typography className={classes.title}>CHOOSE YOUR DIP</Typography>
       <Box display="flex" justifyContent='center' mt="50px">
         {imgArr.map((elem, index) => {
-          return <Box key={index} className={classes.section}>
+          return <Box key={index} className={classes.section} id={elem.id} onClick={chooseDip}>
             <img className={classes.dip} src={elem.url1}></img>
             <img className={classes.bowl} src={elem.url2}></img>
+            <Typography className={classes.chipTitle} style={{ color: elem.color }}>{elem.id}</Typography>
           </Box>
         })}
       </Box>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box className={classes.paper}>
+            {!dipped ? (
+              <>
+                <Typography className={classes.selectModalTitle}>
+                  Please select each chip you would like to dip in <span style={{ textTransform: 'capitalize' }}>{selectedDip}</span>
+                </Typography>
+                <FormControl component="fieldset">
+                  <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                    <FormControlLabel value="873" control={<CustomRadio />} label={<Typography className={classes.customLabel}>Chip #873</Typography>} />
+                    <FormControlLabel value="44" control={<CustomRadio />} label={<Typography className={classes.customLabel}>Chip #44</Typography>} />
+                    <FormControlLabel value="1222" control={<CustomRadio />} label={<Typography className={classes.customLabel}>Chip #1222</Typography>} />
+                  </RadioGroup>
+                </FormControl>
+                <Box mt="20px" display='flex' justifyContent='center'>
+                  <Button variant="contained" color="primary" onClick={dipChip}>
+                    Dip it
+                  </Button>
+                </Box>
+              </>
+            ) : (
+              <>
+                <Typography className={classes.selectModalTitle}>
+                  You dipped chip #{value};
+                </Typography>
+                <Typography className={classes.selectModalTitle}>
+                  You have two more chips available to dip this session
+                </Typography>
+                <Box mt="20px" display='flex' justifyContent='center'>
+                  <Button variant="contained" color="primary" onClick={() => setDipped(false)}>
+                    Tasty
+                  </Button>
+                </Box>
+              </>
+            )}
+          </Box>
+        </Fade>
+      </Modal>
     </Container>
   );
 };
--- theme.js
+++ theme.js
@@ -13,7 +13,7 @@ const theme = createTheme({
       blue: blue,
     },
     primary: {
-      main: mainBlack,
+      main: "#a7cf3a",
     },
     secondary: {
       main: mainWhite,
@@ -53,14 +53,6 @@ const theme = createTheme({
     },
   },
   overrides: {
-    MuiCheckbox: {
-      root: {
-        color: "#000",
-        '&$checked': {
-          color: "#000",
-        },
-      }
-    },
     MuiTypography: {
       body1: {
         fontFamily: "Poppins",
@@ -74,79 +66,12 @@ const theme = createTheme({
         zIndex: 1
       }
     },
-    MuiMenu: {
-      paper: {
-        marginTop: "5px",
-        background: "#2A2B35",
-        border: "1px solid #55576A",
-        boxSizing: "border-box",
-        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        borderRadius: '3px',
-      },
-      list: {
-        padding: "0"
-      }
-    },
-    MuiTooltip: {
-      tooltipPlacementTop: {
-        fontFamily: "Poppins",
-        fontSize: "16px !important",
-        lineHeight: "21px",
-        fontWeight: "600",
-        fontStyle: "normal",
-        color: "#F1F1F1 !important",
-        backgroundColor: "#30B943 !important",
-        border: "0 !important",
-        margin: "0 !important"
-      },
-      arrow: {
-        color: "#30B943"
-      }
-    },
-    MuiAccordion: {
-      root: {
-        border: "1px solid #55576A",
-        background: "#31323E"
-      }
-    },
-    MuiAccordionSummary: {
-      content: {
-        margin: "0",
-        "&.Mui-expanded": {
-          margin: "0",
-        }
-      },
-    },
-    MuiListItem: {
-      button: {
-        "&:hover": {
-          color: "#FFFFFF",
-          backgroundColor: "rgba(85, 87, 106, 0.3)"
-        }
-      },
-      root: {
-        "&.Mui-selected": {
-          background: "#2A2B35",
-        },
-        "&.Mui-selected:hover": {
-          background: "#2A2B35"
-        },
-      }
-    },
-    MuiMenuItem: {
-      root: {
-        fontFamily: "Poppins",
-        fontSize: "18px",
-        lineHeight: "23px",
-        fontWeight: "500",
-        color: "#A4A6A4",
-        paddingTop: "13px",
-        paddingBottom: "13px"
-      },
-    },
     MuiButton: {
       root: {
-        fontFamily: "Poppins"
+        fontFamily: "Poppins",
+        border: '3px solid #000f47',
+        minWidth: '150px',
+        fontWeight: 'bold'
       }
     }
   }
