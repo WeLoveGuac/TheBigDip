49fd44df36477f2c12afca20142650f8813f51ab
smart1206 <proharvester@outlook.com>
2022-09-29 12:40:12 -0500

    updated connect wallet logic

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -189,6 +189,40 @@ const Header = () => {
       toast.error('Please install and active the metamask extension!');
       return;
     }
+
+    try {
+      // check if the chain to connect to is installed
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x89' }], // chainId must be in hexadecimal numbers
+      });
+    } catch (error) {
+      // This error code indicates that the chain has not been added to MetaMask
+      // if it is not, then install it into the user MetaMask
+      if (error.code === 4902) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainName: 'Polygon Mainnet!',
+                chainId: '0x89',
+                rpcUrls: ['https://polygon-rpc.com'],
+                nativeCurrency: {
+                  symbol: 'MATIC',
+                  decimals: 18
+                },
+                blockExplorerUrls: ['https://polygonscan.com']
+              },
+            ],
+          });
+        } catch (addError) {
+          console.error(addError);
+        }
+      }
+      console.error(error);
+    }
+
     const networkId = await ethereum.request({
       method: "net_version",
     });
